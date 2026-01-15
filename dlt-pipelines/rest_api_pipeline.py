a65db2a0008e2413947cdf3605a79e04a9dc067b
GscDTAnalytic <GscDataAnalytic@icloud.com>
2026-01-15 14:44:15 -0300

    adicionar gênero e faixa etária à carga inicial e implementar carga incremental no Magento

--- dlt-pipelines/rest_api_pipeline.py
+++ dlt-pipelines/rest_api_pipeline.py
@@ -6,7 +6,21 @@ from dlt.sources.rest_api import rest_api_resources
 from dlt.sources.rest_api.typing import RESTAPIConfig
 from dlt.sources.helpers.rest_client.paginators import PageNumberPaginator
 import requests
+import logging
+from datetime import datetime
+import urllib3
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler('magento_pipeline.log')
+    ]
+)
+logger = logging.getLogger(__name__)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 custom_session = requests.Session()
 custom_session.verify = False
@@ -19,7 +33,7 @@ def magento_source() -> Any:
             "base_url": "https://magento.test/rest/V1/",
             "auth": {
                 "type": "bearer",
-                "token": dlt.secrets["magento_access_token"],
+                "token": "xqhcm6bgr0ox036pk93ocmnlz7izo9ma",
             },
             "session": custom_session,
         },
@@ -32,7 +46,9 @@ def magento_source() -> Any:
                 },
                 "data_selector": "items",
                 "paginator": PageNumberPaginator(
-                    page_param="searchCriteria[currentPage]", total_path="total_count"
+                    base_page=1,
+                    page_param="searchCriteria[currentPage]",
+                    total_path="total_count",
                 ),
             },
         },
@@ -41,6 +57,84 @@ def magento_source() -> Any:
                 "name": "orders",
                 "endpoint": {
                     "path": "orders",
+                    "params": {
+                        "searchCriteria[sortOrders][0][field]": "entity_id",
+                        "searchCriteria[sortOrders][0][direction]": "ASC",
+                        # Carga incremental baseado em updated_at
+                        "searchCriteria[filter_groups][0][filters][0][field]": "updated_at",
+                        "searchCriteria[filter_groups][0][filters][0][condition_type]": "gteq",
+                        "searchCriteria[filter_groups][0][filters][0][value]":  {
+                            "type": "incremental",
+                            "cursor_path": "updated_at",
+                            "initial_value": "2020-01-01 00:00:00",
+                        },
+                    },    
+                },
+            },
+
+            {
+                "name": "products",
+                "primary_key": "id",
+                "write_disposition": "merge",
+                "endpoint": {
+                    "path": "products",
+                    "params": {
+                        "searchCriteria[pageSize]": 100,
+                        "searchCriteria[sortOrders][0][field]": "id",
+                        "searchCriteria[sortOrders][0][direction]": "ASC",
+                        # Carga incremental baseado em updated_at
+                        "searchCriteria[filter_groups][0][filters][0][field]": "updated_at",
+                        "searchCriteria[filter_groups][0][filters][0][condition_type]": "gteq",
+                        "searchCriteria[filter_groups][0][filters][0][value]": {
+                            "type": "incremental",
+                            "cursor_path": "updated_at",
+                            "initial_value": "2020-01-01 00:00:00",
+                        },
+                    },
+                    "data_selector": "items",
+                    "paginator": PageNumberPaginator(
+                        base_page=1,
+                        page_param="searchCriteria[currentPage]",
+                        total_path="total_count",
+                    ),
+                },
+            },
+
+            {
+                "name": "customers",
+                "primary_key": "id",
+                "endpoint": {
+                    "path": "customers/search",
+                    "params": {
+                        "searchCriteria[sortOrders][0][field]": "id",
+                        "searchCriteria[sortOrders][0][direction]": "ASC",
+                        "searchCriteria[filter_groups][0][filters][0][field]": "updated_at",
+                        "searchCriteria[filter_groups][0][filters][0][condition_type]": "gteq",
+                        "searchCriteria[filter_groups][0][filters][0][value]":  {
+                            "type": "incremental",
+                            "cursor_path": "updated_at",
+                            "initial_value": "2020-01-01 00:00:00",
+                        },
+                    },    
+                },
+            },
+            {
+                "name": "categories",
+                "primary_key": "id",
+                "write_disposition": "merge",
+                "endpoint": {
+                    "path": "categories/list",
+                    "params": {
+                        "searchCriteria[sortOrders][0][field]": "id",
+                        "searchCriteria[sortOrders][0][direction]": "ASC",
+                        "searchCriteria[filter_groups][0][filters][0][field]": "updated_at",
+                        "searchCriteria[filter_groups][0][filters][0][condition_type]": "gteq",
+                        "searchCriteria[filter_groups][0][filters][0][value]":  {
+                            "type": "incremental",
+                            "cursor_path": "updated_at",
+                            "initial_value": "2020-01-01 00:00:00",
+                        },
+                    }    
                 },
             },
         ],
